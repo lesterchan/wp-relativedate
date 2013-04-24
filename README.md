ebae46e7fe006a801042d58e8b0326aaa6779ab4
Lester Chan <lesterchan@gmail.com>
2013-04-24 17:13:05 +0800

    first commit to wp-relativedate

--- README.md
+++ README.md
@@ -1,4 +1,121 @@
-wp-relativedate
-===============
+# WP-RelativeDate
+Contributors: GamerZ
+Donate link: http://lesterchan.net/site/donation/
+Tags: date, relative, relativedate, days, ago, weeks, since, hours, seconds, minutes, today, yesterday
+Requires at least: 2.8
+Tested up to: 3.5
+Stable tag: trunk
 
-Displays relative date alongside with your post/comments actual date. Like 'Today', 'Yesterday', '2 Days Ago', '2 Weeks Ago', '2 'Seconds Ago', '2 Minutes Ago', '2 Hours Ago'.
+Displays relative date alongside with your post/comments actual date.
+
+## Description
+
+Like 'Today', 'Yesterday', '2 Days Ago', '2 Weeks Ago', '2 'Seconds Ago', '2 Minutes Ago', '2 Hours Ago'.
+
+### Previous Versions
+* [WP-RelativeDate 1.40 For WordPress 2.1.x To 2.7.x](http://downloads.wordpress.org/plugin/wp-relativedate.1.40.zip "WP-RelativeDate 1.40 For WordPress 2.1.x To 2.7.x")
+* [WP-RelativeDate 1.00 For WordPress 2.0.x](http://downloads.wordpress.org/plugin/wp-relativedate.1.00.zip "WP-RelativeDate 1.00 For WordPress 2.0.x")
+
+### Development
+* [http://dev.wp-plugins.org/browser/wp-relativedate/](http://dev.wp-plugins.org/browser/wp-relativedate/ "http://dev.wp-plugins.org/browser/wp-relativedate/")
+
+### Translations
+* [http://dev.wp-plugins.org/browser/wp-relativedate/i18n/](http://dev.wp-plugins.org/browser/wp-relativedate/i18n/ "http://dev.wp-plugins.org/browser/wp-relativedate/i18n/")
+
+### Support Forums
+* [http://forums.lesterchan.net/index.php?board=19.0](http://forums.lesterchan.net/index.php?board=19.0 "http://forums.lesterchan.net/index.php?board=19.0")
+
+### Donations
+* I spent most of my free time creating, updating, maintaining and supporting these plugins, if you really love my plugins and could spare me a couple of bucks, I will really appericiate it. If not feel free to use it without any obligations.
+
+## Changelog
+
+### Version 1.50 (01-06-2009)
+* NEW: Use _n() Instead Of __ngettext() And _n_noop() Instead Of __ngettext_noop()
+
+### Version 1.40 (12-12-2008)
+* FIXED: Pass In Default Values For $d, $before And $after In relative_post_date()
+
+### Version 1.31 (16-07-2008)
+* NEW: Works For WordPress 2.6
+* NEW: Better Translation Using __ngetext() by <a href="http://hweia.ru/" title="http://hweia.ru/">Anna Ozeritskaya</a>
+
+### Version 1.30 (01-06-2008)
+* NEW: Uses /wp-relativedate/ Folder Instead Of /relativedate/
+* NEW: Uses wp-relativedate.php Instead Of relativedate.php
+
+### Version 1.20 (01-10-2007)
+* New: relative_post_the_date(); Alternative To WordPress the_date()
+
+### Version 1.11 (01-06-2007)
+* FIXED: Post Of The Same Date But Different Year Still Will Not Display Relative Date
+
+### Version 1.10 (01-02-2007)
+* NEW: Works For WordPress 2.1 Only
+* NEW: Localization WP-RelativeDate
+
+### Version 1.00 (01-03-2006)
+* NEW: Initial Release
+
+## Installation
+
+1. Open `wp-content/plugins` Folder
+2. Put: `Folder: wp-relativedate`
+3. Activate `WP-RelativeDate` Plugin
+
+### General Usage
+* You need not do anything. WP-RelativeDate will automatically modify your post/comment date or time display. It will add the following text accordingly:
+ * Post/Comment Date
+```
+Today
+Yesterday
+X days ago
+X weeks ago
+```
+ * Post/Comment Time
+```
+X seconds ago
+X minutes ago
+X hours ago
+```
+
+* Examples are shown below:
+ * Post/Comment Date
+```
+Today
+Yesterday
+10th January 2007 (2 days ago)
+25th January 2007 (2 weeks ago)
+```
+ * Post/Comment Time
+```
+21:10 (2 seconds ago)
+21:15 (5 minutes ago)
+22:15 (2 hours ago)
+```
+
+* No text will be added if the post or comment is more than a month old.
+
+## Upgrading
+
+1. Deactivate `WP-RelativeDate` Plugin
+2. Open `wp-content/plugins` Folder
+3. Put/Overwrite: `Folder: wp-relativedate`
+4. Activate `WP-RelativeDate` Plugin
+
+## Upgrade Notice
+
+N/A
+
+## Screenshots
+
+1. Comment - Seconds Ago
+2. Post - Days Ago
+3. Post - Today
+4. Post - Weeks Ago
+5. Post - Yesterday
+
+## Frequently Asked Questions
+
+### Display Relative Date in every posts
+* If you want to display Relative Date in every posts, use `relative_post_the_date()` instead of `the_date()` in your theme.
---
+++ wp-relativedate.php
@@ -0,0 +1,209 @@
+<?php
+/*
+Plugin Name: WP-RelativeDate
+Plugin URI: http://lesterchan.net/portfolio/programming/php/
+Description: Displays relative date alongside with your post/comments actual date. Like 'Today', 'Yesterday', '2 Days Ago', '2 Weeks Ago', '2 'Seconds Ago', '2 Minutes Ago', '2 Hours Ago'.
+Version: 1.50
+Author: Lester 'GaMerZ' Chan
+Author URI: http://lesterchan.net
+Text Domain: wp-relativedate
+*/
+
+
+/*
+	Copyright 2013  Lester Chan  (email : lesterchan@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+### Create Text Domain For Translations
+add_action('init', 'relativedate_textdomain');
+function relativedate_textdomain() {
+	if (!function_exists('wp_print_styles')) {
+		load_plugin_textdomain('wp-relativedate', 'wp-content/plugins/wp-relativedate');
+	} else {
+		load_plugin_textdomain('wp-relativedate', false, 'wp-relativedate');
+	}
+}
+
+
+### Function: Display Post Relative Date (Today/Yesterday/Days Ago/Weeks Ago)
+add_filter('the_date', 'relative_post_date', 999, 4);
+function relative_post_date($the_date, $d = '', $before = '', $after = '', $display_ago_only = false) {
+	global $post, $previous_day;
+	$the_date = strip_tags($the_date);
+	if(gmdate('Y', current_time('timestamp')) != mysql2date('Y', $post->post_date, false) && !empty($the_date)) {
+		return $before.$the_date.$after;
+	}
+	$day_diff = (gmdate('z', current_time('timestamp')) - mysql2date('z', $post->post_date, false));
+	if($day_diff < 0) { $day_diff = 32; }
+	if ($the_date != $previous_day) {
+		if($day_diff == 0) {
+			return $before.__('Today', 'wp-relativedate').$after;
+		} elseif($day_diff == 1) {
+			return $before. __('Yesterday', 'wp-relativedate').$after;
+		} elseif ($day_diff < 7) {
+			if($display_ago_only) {
+				return $before.sprintf(_n('%s day ago', '%s days ago', $day_diff, 'wp-relativedate'), number_format_i18n($day_diff)).$after;
+			} else {
+				return $before.$the_date.' ('.sprintf(_n('%s day ago', '%s days ago', $day_diff, 'wp-relativedate'), number_format_i18n($day_diff)).')'.$after;
+			}
+		} elseif ($day_diff < 31) {
+			if($display_ago_only) {
+				return $before.sprintf(_n('%s week ago', '%s weeks ago', ceil($day_diff/7), 'wp-relativedate'), number_format_i18n(ceil($day_diff/7))).$after;
+			} else {
+				return $before.$the_date.' ('.sprintf(_n('%s week ago', '%s weeks ago', ceil($day_diff/7), 'wp-relativedate'), number_format_i18n(ceil($day_diff/7))).')'.$after;
+			}
+		} else {
+			return $before.$the_date.$after;
+		}
+		$previous_day = $the_date;
+	}
+}
+
+
+### Alternative To WordPress the_date().
+function relative_post_the_date($d = '', $before = '', $after = '', $display_ago_only = false, $display = true) {
+	global $post;
+	if (empty($d)) {
+		$the_date = mysql2date(get_option('date_format'), $post->post_date);
+	} else {
+		$the_date = mysql2date($d, $post->post_date);
+	}
+	$output = '';
+	if(gmdate('Y', current_time('timestamp')) != mysql2date('Y', $post->post_date, false)) {
+		$output = $before.$the_date.$after;
+	} else {
+		$day_diff = (gmdate('z', current_time('timestamp')) - mysql2date('z', $post->post_date, false));
+		if($day_diff < 0) { $day_diff = 32; }
+		if($day_diff == 0) {
+			$output = $before.__('Today', 'wp-relativedate').$after;
+		} elseif($day_diff == 1) {
+			$output = $before. __('Yesterday', 'wp-relativedate').$after;
+		} elseif ($day_diff < 7) {
+			if($display_ago_only) {
+				$output = $before.sprintf(_n('%s day ago', '%s days ago', $day_diff, 'wp-relativedate'), number_format_i18n($day_diff)).$after;
+			} else {
+				$output = $before.$the_date.' ('.sprintf(_n('%s day ago', '%s days ago', $day_diff, 'wp-relativedate'), number_format_i18n($day_diff)).')'.$after;
+			}
+		} elseif ($day_diff < 31) {
+			if($display_ago_only) {
+				$output = $before.sprintf(_n('%s week ago', '%s weeks ago', ceil($day_diff/7), 'wp-relativedate'), number_format_i18n(ceil($day_diff/7))).$after;
+			} else {
+				$output = $before.$the_date.' ('.sprintf(_n('%s week ago', '%s weeks ago', ceil($day_diff/7), 'wp-relativedate'), number_format_i18n(ceil($day_diff/7))).')'.$after;
+			}
+		} else {
+			$output = $before.$the_date.$after;
+		}
+	}
+	if($display) {
+		echo $output;
+	} else {
+		return $output;
+	}
+}
+
+
+### Function: Display Post Relative Time (Seconds Ago/Minutes Ago/Hours Ago)
+add_filter('the_time', 'relative_post_time', 999);
+function relative_post_time($current_timeformat, $display_ago_only = 0) {
+  global $post;
+	$current_time = current_time('timestamp');
+	$date_today_time = gmdate('j-n-Y H:i:s', $current_time);
+	$post_date_time = mysql2date('j-n-Y H:i:s', $post->post_date, false);
+	$date_today = gmdate('j-n-Y', $current_time);
+	$post_date = mysql2date('j-n-Y', $post->post_date, false);
+	$time_diff = (strtotime($date_today_time) - strtotime($post_date_time));
+	$format_ago = '';
+	if($post_date == $date_today) {
+		if($time_diff < 60) {
+			$format_ago = sprintf(_n('%s second ago', '%s seconds ago', $time_diff, 'wp-relativedate'), number_format_i18n($time_diff));
+		} elseif ($time_diff < 3600) {
+			$format_ago = sprintf(_n('%s minute ago', '%s minutes ago', intval($time_diff/60), 'wp-relativedate'), number_format_i18n(intval($time_diff/60)));
+		} elseif ($time_diff < 86400) {
+			$format_ago = sprintf(_n('%s hour ago', '%s hours ago', intval($time_diff/3600), 'wp-relativedate'), number_format_i18n(intval($time_diff/3600)));
+		}
+		if($display_ago_only) {
+			return $format_ago;
+		} else {
+			return $current_timeformat.' ('.$format_ago.')';
+		}
+	} else {
+		return $current_timeformat;
+	}
+}
+
+
+### Function: Display Comment Relative Date (Today/Yesterday/Days Ago/Weeks Ago)
+add_filter('get_comment_date', 'relative_comment_date', 999);
+function relative_comment_date($current_dateformat, $display_ago_only = 0) {
+	global $comment;
+	$comment_date = $comment->comment_date;
+	if(gmdate('Y', current_time('timestamp')) != mysql2date('Y', $comment_date, false)) {
+		return $current_dateformat;
+	}
+	$day_diff = (gmdate('z', current_time('timestamp')) - mysql2date('z', $comment_date, false));
+	if($day_diff < 0) { $day_diff = 32; }
+	if($day_diff == 0) {
+		return __('Today', 'wp-relativedate');
+	} elseif($day_diff == 1) {
+		return __('Yesterday', 'wp-relativedate');
+	} elseif ($day_diff < 7) {
+		if($display_ago_only) {
+			return sprintf(_n('%s day ago', '%s days ago', $day_diff, 'wp-relativedate'), number_format_i18n($day_diff));
+		} else {
+			return $current_dateformat.' ('.sprintf(_n('%s day ago', '%s days ago', $day_diff, 'wp-relativedate'), number_format_i18n($day_diff)).')';
+		}
+	} elseif ($day_diff < 31) {
+		if($display_ago_only) {
+			return sprintf(_n('%s week ago', '%s weeks ago', ceil($day_diff/7), 'wp-relativedate'), number_format_i18n(ceil($day_diff/7)));
+		} else {
+			return $current_dateformat.' ('.sprintf(_n('%s week ago', '%s weeks ago', ceil($day_diff/7), 'wp-relativedate'), number_format_i18n(ceil($day_diff/7))).')';
+		}
+	} else {
+		return $current_dateformat;
+	}
+}
+
+
+### Function: Display Comment  Relative Time (Seconds Ago/Minutes Ago/Hours Ago)
+add_filter('get_comment_time', 'relative_comment_time', 999);
+function relative_comment_time($current_timeformat, $display_ago_only = 0) {
+	global $comment;
+	$current_time = current_time('timestamp');
+	$date_today_time = gmdate('j-n-Y H:i:s', $current_time);
+	$comment_date_time = mysql2date('j-n-Y H:i:s', $comment->comment_date, false);
+	$date_today = gmdate('j-n-Y', $current_time);
+	$comment_date = mysql2date('j-n-Y', $comment->comment_date, false);
+	$time_diff = (strtotime($date_today_time) - strtotime($comment_date_time));
+	$format_ago = '';
+	if($comment_date == $date_today) {
+		if($time_diff < 60) {
+			$format_ago = sprintf(_n('%s second ago', '%s seconds ago', $time_diff, 'wp-relativedate'), number_format_i18n($time_diff));
+		} elseif ($time_diff < 3600) {
+			$format_ago = sprintf(_n('%s minute ago', '%s minutes ago', intval($time_diff/60), 'wp-relativedate'), number_format_i18n(intval($time_diff/60)));
+		} elseif ($time_diff < 86400) {
+			$format_ago = sprintf(_n('%s hour ago', '%s hours ago', intval($time_diff/3600), 'wp-relativedate'), number_format_i18n(intval($time_diff/3600)));
+		}
+		if($display_ago_only) {
+			return $format_ago;
+		} else {
+			return $current_timeformat.' ('.$format_ago.')';
+		}
+	} else {
+		return $current_timeformat;
+	}
+}
+?>
